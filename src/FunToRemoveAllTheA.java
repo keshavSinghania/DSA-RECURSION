6b78f12c69d561b8ad8375feae515d25c7514556
Keshav Singhania <worksinghania@gmail.com>
2025-09-26 22:13:27 +0530

    h

---
+++ src/FunToRemoveAllTheA.java
@@ -0,0 +1,28 @@
+public class FunToRemoveAllTheA {
+    public static void main(String[] args){
+        String str = "aaakeaaashava";
+        StringBuilder sb = new StringBuilder();
+//        sb = removeA(str);
+        System.out.println(sb);
+    }
+
+//    //function to remove a from string using simple loop
+//    public static StringBuilder removeA(String str){
+//        StringBuilder sb = new StringBuilder();
+//        for(int i=0; i<str.length(); i++){
+//            if(str.charAt(i) != 'a'){
+//                sb.append(str.charAt(i));
+//            }
+//        }
+//        return sb;
+//    }
+
+    //function to remove a from string using recursion
+    public static void removeA(String str, StringBuilder sb){
+        StringBuilder sb1 = new StringBuilder();
+        //base condition
+        if(str.length() == 0){
+            return ;
+        }
+    }
+}
