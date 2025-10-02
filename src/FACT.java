65bece51c2502ec4812dc59ce83268c115671527
Keshav Singhania <worksinghania@gmail.com>
2025-10-02 22:40:15 +0530

    Fact

---
+++ src/FACT.java
@@ -0,0 +1,16 @@
+//public class FACT {
+//}
+//// Write a recursive function to find the factorial of a number.
+//class Main {
+//    public static void main(String[] args) {
+//        int n = 4;
+//        System.out.println(fact(n));
+//    }
+//
+//    //function to find factorial of a number
+//    static int fact(int n){
+//        if(n==1) return 1;
+//
+//        return n * fact(n-1);
+//    }
+//}
